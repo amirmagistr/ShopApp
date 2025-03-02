91dd4c44d9549ac58c8700f9392924eedcecf6d0
amir <amir.temirbulatov@narxoz.kz>
2025-03-02 21:57:12 +0500

    add Clothing model

---
+++ src/main/java/org/example/model/Clothing.java
@@ -0,0 +1,52 @@
+package org.example.model;
+
+public class Clothing {
+    private final int id;
+    private String name;
+    private int price;
+    private String category;
+
+    public Clothing(int id, String name, int price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    @Override
+    public String toString() {
+        return "Clothing{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
